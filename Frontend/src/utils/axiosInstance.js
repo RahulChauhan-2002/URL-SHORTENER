d0606b79a849ed650516cb314cd3c57d4303d6f9
Rahul Chauhan <rahulchauhan962103@gmail.com>
2025-07-11 18:11:41 +0530

    Update backend API baseURL

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const axiosInstance=axios.create({
-    baseURL:"https://url-shortener-10u2.onrender.com",
-    timeout:10000,
-    withCredentials: true
+const axiosInstance = axios.create({
+  baseURL: "https://url-shortener-nxc8.onrender.com",
+  timeout: 10000,
+  withCredentials: true
 });
+
 export default axiosInstance;
